504fd044ab36132e8f016171d1c5450ccba65d60
snikt094 <shawhin022@gmail.com>
2025-01-06 14:15:34 -0500

    Much stuff

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.remap")
 require("config.lazy")
 require("config.options")
+require("config.tsutils")
--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,14 +3,12 @@ return {
         "lervag/vimtex",
         lazy = false,     -- we don't want to lazy load VimTeX
         config = function()
-            -- VimTeX configuration goes here, e.g.
-            vim.g.vimtex_view_method = "zathura"
             vim.g.tex_flavor = 'latex'
-            vim.g.vimtex_quickfix_mode = 1
-            vim.g.conceallevel=1
-            vim.g.tex_conceal='abdmg'
-        
---            vim.g.syntax_conceal_disable = 1
+            vim.g.vimtex_view_method = 'zathura'
+            vim.g.vimtex_quickfix_mode = 0
+            vim.g.tex_conceal = 'abdmgs'
+
+            vim.opt.conceallevel = 1
         end,
     }
 }
