4712a5a033a2c9f81c9e2986544199101e46ab1b
snikt094 <shawhin022@gmail.com>
2025-03-12 13:01:40 -0400

    some commit

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,11 @@ if status is-interactive
     alias op="zathura (find . -iname '*.pdf' | fzf -m) &" # Open some pdfs in cwd 
     alias oap="zathura (find ~ -iname '*.pdf' | fzf -m) &" # Open any pdfs 
 
-    alias od="cd (find ~ -type d | fzf)" # Open a directory
+    alias odn="cd (find ~ -type d | fzf) && nvim ." # Open a directory in nvim
+    alias od="cd (find ~ -type d | fzf)" # Open a directory 
     alias of="nvim (find ~ -type f | fzf -m)" # Open files
-    alias odu="cd (find ~/Documents/University -type d | fzf)" # Open a directory in for university
-    alias ofu="nvim (find ~/Documents/University -type f | fzf -m)" # Open a university files
+    alias odu="cd (find ~/Documents/university -type d | fzf)" # Open a directory in for university
+    alias ofu="nvim (find ~/Documents/university -type f | fzf -m)" # Open a university files
 
     abbr -a fd --position anywhere "(find ~ -type d -not -path '*/.*' |fzf)" # Find a Directory 
     abbr -a ff --position anywhere "(find ~ -type f -not -path '*/.*' |fzf -m)" # Find a file 
--- nvim/lua/plugins/ultisnips.lua
+++ nvim/lua/plugins/ultisnips.lua
@@ -4,9 +4,9 @@ return {
         lazy = false,
         enabled = true, 
         config = function()
-            vim.g.UltiSnipsExpandTrigger = "<tab>"
+            vim.g.UltiSnipsExpandTrigger = "<CR>"
             vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
-            vim.g.UltiSnipsJumpBackwardTrigger = "<c-a>"
+            vim.g.UltiSnipsJumpBackwardTrigger = "<c-s>"
             vim.g.UltisnipsSnippetStorageDirectoryForUltiSnipsEdit = "~/.config/nvim/UltiSnips"
         end
     }
