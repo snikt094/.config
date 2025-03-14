d4cea144cc833be680958bc385efe82b49155484
snikt094 <shawhin022@gmail.com>
2025-03-14 16:22:25 -0400

    Updated fzf stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias ob="zathura (find ~/Documents/University/Books/ -iname '*.pdf' | fzf -m) &" # Open a textbooks
+    alias ob="zathura (find ~/Documents/university/Books/ -iname '*.pdf' | fzf -m) &" # Open a textbooks
     alias op="zathura (find . -iname '*.pdf' | fzf -m) &" # Open some pdfs in cwd 
-    alias oap="zathura (find ~ -iname '*.pdf' | fzf -m) &" # Open any pdfs 
+    #    alias oap="zathura (find ~ -iname '*.pdf' | fzf -m) &" # Open any pdfs 
 
-    alias odn="cd (find ~ -type d | fzf) && nvim ." # Open a directory in nvim
     alias od="cd (find ~ -type d | fzf)" # Open a directory 
-    alias of="nvim (find ~ -type f | fzf -m)" # Open files
     alias odu="cd (find ~/Documents/university -type d | fzf)" # Open a directory in for university
-    alias ofu="nvim (find ~/Documents/university -type f | fzf -m)" # Open a university files
+    alias odn="cd (find ~ -type d | fzf) && nvim ." # Open a directory in nvim
+    alias of="nvim (find . -type f | fzf -m)" # Open files
+    #    alias ofu="nvim (find ~/Documents/university -type f | fzf -m)" # Open a university files
 
     abbr -a fd --position anywhere "(find ~ -type d -not -path '*/.*' |fzf)" # Find a Directory 
     abbr -a ff --position anywhere "(find ~ -type f -not -path '*/.*' |fzf -m)" # Find a file 
--- nvim/lua/plugins/ultisnips.lua
+++ nvim/lua/plugins/ultisnips.lua
@@ -4,7 +4,7 @@ return {
         lazy = false,
         enabled = true, 
         config = function()
-            vim.g.UltiSnipsExpandTrigger = "<CR>"
+            vim.g.UltiSnipsExpandTrigger = "<tab>"
             vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
             vim.g.UltiSnipsJumpBackwardTrigger = "<c-s>"
             vim.g.UltisnipsSnippetStorageDirectoryForUltiSnipsEdit = "~/.config/nvim/UltiSnips"
