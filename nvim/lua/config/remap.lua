db99f1be245b1546dba42e50cd4816bd5bf511ac
snikt094 <shawhin022@gmail.com>
2025-03-19 01:56:12 -0400

    Some new remaps

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -3,13 +3,18 @@ vim.g.localleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set('i', "<C-l>", '<c-g>u<Esc>[s1z=`]a<c-g>u') --Correct last mistake
+ 
 vim.keymap.set("n", "<leader>s", vim.cmd.w) --Save file 
 vim.keymap.set("n", "<leader>a", vim.cmd.wq) --Save and quite file 
---vim.keymap.set("n", "<leader>q", vim.cmd.q!) --Quite file without saving 
+vim.keymap.set("n", "<leader>q", vim.cmd.q) --Quite
+
+vim.keymap.set("v", "h", "^") --Start of line
+vim.keymap.set("v", "l", "$") --End of line
 
 -- Move highlighted text 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("x", "<leader>p", [["_dP]])
